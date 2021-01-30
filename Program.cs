da451d14077bb3c35a5ab642098d8d8efcf205f8
james tiu <jamestiulawyer@gmail.com>
2021-01-29 20:18:08 -0700

    inital commit, from scratch Jan 29

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScratchClockRadioJan29
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, time to rise & shine with a Scratch Clock Radio!!!");
+            Console.WriteLine("");
+            ScratchClockRadio myclockAlarmRadio = new ScratchClockRadio("6:00 a.m.", "102.5", true, "6:35 a.m.");
+            Console.WriteLine("Your clock says that the time is: " + myclockAlarmRadio.time);
+            Console.WriteLine("");
+            Console.WriteLine("I set the radio to your favorite FM station, " + myclockAlarmRadio.radioStation + ", and set the alarm for " + myclockAlarmRadio.alarmTime);
+            Console.WriteLine("");
+            bool alarmOn = true;
+            if (alarmOn)
+            {
+                Console.WriteLine("I turned your alarm 'ON' ... I know it's early!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ScratchClockRadio.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScratchClockRadioJan29
+{
+    public class ScratchClockRadio
+    {
+        //Variables HAS A: Digital Read Out with time, current radio station, Allarm On/Off, Alarm Time 
+
+        public string time;
+        public string radioStation;
+        public bool alarmOn;
+        public string alarmTime;
+
+        public ScratchClockRadio (string time, string radioStation, bool alarmnOn, string alarmTime)
+        {
+            
+
+            this.time = time;
+            this.radioStation = radioStation;
+            this.alarmOn = alarmnOn;
+            this.alarmTime = alarmTime;
+
+        }
+         
+
+
+    //Constructor SPAWN
+
+
+
+
+    //Methods  CAN DO
+
+
+    }
+}
